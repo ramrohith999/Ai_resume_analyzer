87c3d17c84f61c38aa42f8533517f9c9277a43dd
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-04-15 17:52:54 +0530

    backend etup done

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("Mongodb conneted"))
+.catch(err=>console.log(err));
+
+app.get("/",(req,res)=>{
+    res.send("API running");
+})
+
+const PORT=5000;
+
+app.listen(PORT, ()=> console.log(`sever running on ${PORT}`));
